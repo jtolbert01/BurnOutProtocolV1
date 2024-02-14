34aeff9c6ca024f154c5805ff5a0c2daa033b1c3
jtolbert01 <59659493+jtolbert01@users.noreply.github.com>
2024-02-13 21:30:47 -0500

    Update main.py

--- assets/main.py
+++ assets/main.py
@@ -1,2 +1,14 @@
+import smtplib
+
+from email.message import EmailMessage
+
 def ask_for_help(event):
+  email_address = "email"
+  email_password = "pass"
+  msg = EmailMessage()
+  msg['Subject'] = "BURNOUT NOTICE"
+  msg['From'] = email_address
+  msg['To'] = "to-address@gmail.com"
+  msg.set_content("You need to cheer up Hazel")
+    
   print("Asking now")
